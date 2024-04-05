63cf9acf8eaead63ac96dcae2e605f8eef552207
B812111027 <161008702+B812111027@users.noreply.github.com>
2024-04-06 02:25:43 +0800

    Update HW2_B812111027.py

--- HW2_B812111027.py
+++ HW2_B812111027.py
@@ -1,27 +1,33 @@
-def count_characters(file_path):
-    char_frequency = {}
+def count_words(file_path):
+    word_frequency = {}
+
 
     try:
         with open(file_path, 'r', encoding='utf-8') as file:
             for line in file:
-                for char in line.strip():
-                    if char.isalpha():
-                        char_lower = char.lower()
-                        char_frequency[char_lower] = char_frequency.get(char_lower, 0) + 1
+                words = line.strip().split()
+                for word in words:
+                    word = ''.join(filter(str.isalpha, word)).lower()
+                    if word:
+                        word_frequency[word] = word_frequency.get(word, 0) + 1
 
-        return char_frequency
+
+        return word_frequency
     except FileNotFoundError:
         print("找不到指定的檔案！")
         return None
 
+
 def main():
     file_path = input("請輸入檔案路徑：")
-    char_frequency = count_characters(file_path)
+    word_freque ncy = count_words(file_path)
 
-    if char_frequency:
-        print("每一個英文字出現次數：")
-        for char, frequency in char_frequency.items():
-            print("%s: %d" % (char, frequency))
 
-if __name__ == "__main__":
+    if word_frequency:
+        print("每一個英文單字出現次數：")
+        for word, frequency in word_frequency.items():
+            print("%s: %d" % (word, frequency))
+
+
+if _ _name_ _ == "_ _main_ _":
     main()
